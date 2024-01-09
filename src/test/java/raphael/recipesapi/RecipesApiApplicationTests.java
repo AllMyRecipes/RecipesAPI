5c46d99a80930f02524e8dc41efc2076d563c30a
raphael <raphael.aldaz@gmail.com>
2024-01-09 08:55:58 +0100

    initializing project

---
+++ src/test/java/raphael/recipesapi/RecipesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package raphael.recipesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
