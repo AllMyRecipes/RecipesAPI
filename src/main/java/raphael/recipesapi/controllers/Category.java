d60567860d040f43bbe592ff9d71f613f69d96c1
raphael <raphael.aldaz@gmail.com>
2024-01-10 09:50:52 +0100

    Updating pom and properties

---
+++ src/main/java/raphael/recipesapi/controllers/Category.java
@@ -0,0 +1,20 @@
+package raphael.recipesapi.controllers;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    private Long id;
+    private String name;
+
+}
