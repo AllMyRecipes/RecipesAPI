b296abd92f3834bf7e89fa1753c178c60f38fcc1
Raphael-Aldaz <raphael.aldaz@gmail.com>
2024-07-09 14:42:15 +0200

    test linux

--- src/main/java/raphael/recipesapi/RecipesApiApplication.java
+++ src/main/java/raphael/recipesapi/RecipesApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecipesApiApplication  {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesApiApplication.class, args);
 	}
